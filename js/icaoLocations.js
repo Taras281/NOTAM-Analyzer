ccde945626ea2c3448d013ac8eaf4bef604019cb
unknown <taras281@yandex.ru>
2026-07-31 22:07:03 +0300

    Support multiple ICAO codes

---
+++ js/icaoLocations.js
@@ -0,0 +1,23 @@
+const icaoLocations = {
+
+    "UUEE": {
+        type: "Airport",
+        name: "Шереметьево",
+        country: "Россия"
+    },
+
+    "EDMM": {
+        type: "FIR",
+        name: "Munich",
+        country: "Germany"
+    },
+
+    "EDGG": {
+        type: "FIR",
+        name: "Langen",
+        country: "Germany"
+    }
+
+};
+
+window.icaoLocations = icaoLocations;
